610f13d0e765582af12168670cd0ec7a881f7aa3
TomFluidly <54806200+TomFluidly@users.noreply.github.com>
2019-10-02 17:05:51 +0100

    Test fix (#5)

--- fluidly-structlog/fluidly/structlog/pubsub_helper.py
+++ fluidly-structlog/fluidly/structlog/pubsub_helper.py
@@ -6,6 +6,41 @@ from fluidly.structlog.base_logger import get_logger
 
 def pubsub_log_entrypoint(func):
     @wraps(func)
+    def wrapper(message, *args, **kwargs):
+        logger = get_logger()
+        logger = logger.new(callback=func.__qualname__)
+        start = time.time()
+
+        try:
+            result = func(message, *args, **kwargs)
+        except Exception:
+            end = time.time()
+            logger.error(
+                "pubsub_message_processed",
+                duration=end - start,
+                success=False,
+                exc_info=True,
+                message=message.data,
+                attributes=message.attributes,
+                connection_id=message.attributes.get("connection_id"),
+            )
+            raise
+
+        end = time.time()
+        logger.info(
+            "pubsub_message_processed",
+            duration=end - start,
+            success=True,
+            attributes=message.attributes,
+            connection_id=message.attributes.get("connection_id"),
+        )
+        return result
+
+    return wrapper
+
+
+def pubsub_log_entrypoint_class(func):
+    @wraps(func)
     def wrapper(self, session, message, *args, **kwargs):
         logger = get_logger()
         logger = logger.new(callback=func.__qualname__)
