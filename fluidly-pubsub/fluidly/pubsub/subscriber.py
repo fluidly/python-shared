8951ada4eb6d514c0e7c66c8c72d4df4831db953
tom <tom.walton@fluidly.com>
2019-10-14 13:03:44 +0100

    Apply formatting

--- fluidly-pubsub/fluidly/pubsub/subscriber.py
+++ fluidly-pubsub/fluidly/pubsub/subscriber.py
@@ -1,5 +1,6 @@
-from fluidly.pubsub.base_subscriber import setup_base_subscriber
 from google.cloud import pubsub_v1
+
+from fluidly.pubsub.base_subscriber import setup_base_subscriber
 
 subscriber = pubsub_v1.SubscriberClient()
 
