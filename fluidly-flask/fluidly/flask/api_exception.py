88c19d41e5924b969afc7d097aff0426e18d2770
Jacek Wojna <gastlich@gmail.com>
2019-10-14 18:26:37 +0100

    Authorised flask decorator

--- fluidly-flask/fluidly/flask/api_exception.py
+++ fluidly-flask/fluidly/flask/api_exception.py
@@ -1,8 +1,26 @@
+import json
+
+from flask import Response
+
+
 class APIException(Exception):
     def __init__(self, status, title, detail=None):
+        """While using APIException don't forget to register custom error
+        handler:
+
+            app.register_error_handler(APIException, handle_api_exception)
+        """
         self.title = title
         self.status = status
         self.detail = detail
 
     def to_dict(self):
         return {"title": self.title, "status": self.status, "detail": self.detail}
+
+
+def handle_api_exception(error):
+    return Response(
+        response=json.dumps(error.to_dict()),
+        status=error.status,
+        mimetype="application/problem+json",
+    )
