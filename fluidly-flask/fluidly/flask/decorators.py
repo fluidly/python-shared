770414b78ee1134bbebc806ee11bfec39f28aa93
tom <tom.walton@fluidly.com>
2019-10-22 11:00:12 +0100

    Add the admin decorator

--- fluidly-flask/fluidly/flask/decorators.py
+++ fluidly-flask/fluidly/flask/decorators.py
@@ -5,6 +5,7 @@ from flask import g, request
 from fluidly.auth.permissions import (
     UserPermissionsPayloadException,
     UserPermissionsRequestException,
+    check_admin_permissions,
     check_user_permissions,
 )
 from fluidly.flask.api_exception import APIException
@@ -47,6 +48,47 @@ def authorised(f):
 
         g.connection_id = connection_id
         g.user_id = user_id
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def admin(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        """Retrieves the authentication information from Google Cloud Endpoints and passes it to user permissions service"""
+        encoded_info = request.headers.get("X-Endpoint-API-UserInfo", None)
+        if not encoded_info:
+            raise APIException(status=401, title="User is not authenticated")
+
+        info_json = base64_decode(encoded_info)
+        # First parsing of the decoded header string
+        user_info = json.loads(info_json)
+        # Claims are given as a string by Cloud Endpoints so we have
+        # to parse the claims attribute
+        claims = json.loads(user_info.get("claims", "{}"))
+
+        auth0_claims = claims.get("https://api.fluidly.com/app_metadata", {})
+        internal_claims = claims.get("https://api.fluidly.com/internal_metadata", {})
+
+        user_id_from_token = auth0_claims.get("userId", None)
+
+        try:
+            given_permissions = check_admin_permissions(claims)
+            is_service_account = internal_claims.get("isServiceAccount", False)
+
+            if not is_service_account and not given_permissions:
+                raise APIException(status=403, title="User cannot access this resource")
+        except (
+            ValueError,
+            UserPermissionsPayloadException,
+            UserPermissionsRequestException,
+        ):
+            raise APIException(
+                status=403, title="An issue occurred while fetching permissions"
+            )
+
+        g.user_id = user_id_from_token
         return f(*args, **kwargs)
 
     return decorated_function
--- fluidly-flask/tests/test_decorators.py
+++ fluidly-flask/tests/test_decorators.py
@@ -3,7 +3,6 @@ import json
 from unittest import mock
 
 import pytest
-from flask import g
 from fluidly.flask import decorators
 
 
@@ -23,6 +22,24 @@ def mocked_given_permissions(monkeypatch):
         decorators, "check_user_permissions", check_user_permissions_mock
     )
     yield check_user_permissions_mock
+
+
+@pytest.fixture
+def mocked_admin_not_given_permissions(monkeypatch):
+    check_admin_permissions_mock = mock.MagicMock(return_value=False)
+    monkeypatch.setattr(
+        decorators, "check_admin_permissions", check_admin_permissions_mock
+    )
+    yield check_admin_permissions_mock
+
+
+@pytest.fixture
+def mocked_admin_given_permissions(monkeypatch):
+    check_admin_permissions_mock = mock.MagicMock(return_value=True)
+    monkeypatch.setattr(
+        decorators, "check_admin_permissions", check_admin_permissions_mock
+    )
+    yield check_admin_permissions_mock
 
 
 class TestAuthorised:
@@ -97,9 +114,6 @@ class TestAuthorised:
         assert response.status_code == 200
         assert response.data == b"some:connection_id"
 
-        assert g.connection_id == "some:connection_id"
-        assert g.user_id == 2
-
     def test_service_account_granted(self, client, mocked_not_given_permissions):
         response = client.get(
             "/shared/authorised/some:connection_id",
@@ -112,5 +126,87 @@ class TestAuthorised:
         assert response.status_code == 200
         assert response.data == b"some:connection_id"
 
-        assert g.connection_id == "some:connection_id"
-        assert g.user_id == None
+
+class TestAdmin:
+    @staticmethod
+    def _encode_claims(claims):
+        return base64.b64encode(json.dumps(claims).encode("utf-8"))
+
+    @staticmethod
+    def _get_dummy_user_info(**kwargs):
+        app_metadata = kwargs.get("app_metadata", {})
+        internal_metadata = kwargs.get("internal_metadata", {})
+
+        return TestAdmin._encode_claims(
+            {
+                "claims": json.dumps(
+                    {
+                        "https://api.fluidly.com/app_metadata": {**app_metadata},
+                        "https://api.fluidly.com/internal_metadata": {
+                            **internal_metadata
+                        },
+                    }
+                )
+            }
+        )
+
+    def test_admin_unauthenticated(self, client):
+        response = client.get("/shared/admin")
+        assert response.status_code == 401
+        assert response.json == {
+            "title": "User is not authenticated",
+            "status": 401,
+            "detail": None,
+        }
+
+    def test_admin_permissions_unavailable(self, client):
+        response = client.get(
+            "/shared/admin",
+            headers={"X-Endpoint-API-UserInfo": TestAdmin._get_dummy_user_info()},
+        )
+        assert response.status_code == 403
+        assert response.json == {
+            "title": "An issue occurred while fetching permissions",
+            "status": 403,
+            "detail": None,
+        }
+
+    def test_admin_permissions_available_not_granted(
+        self, client, mocked_admin_not_given_permissions
+    ):
+        response = client.get(
+            "/shared/admin",
+            headers={"X-Endpoint-API-UserInfo": TestAdmin._get_dummy_user_info()},
+        )
+        assert response.status_code == 403
+        assert response.json == {
+            "title": "User cannot access this resource",
+            "status": 403,
+            "detail": None,
+        }
+
+    def test_admin_permissions_available_granted(
+        self, client, flask_app, mocked_admin_given_permissions
+    ):
+        response = client.get(
+            "/shared/admin",
+            headers={
+                "X-Endpoint-API-UserInfo": TestAdmin._get_dummy_user_info(
+                    app_metadata={"userId": 2}
+                )
+            },
+        )
+        assert response.status_code == 200
+
+    def test_admin_service_account_granted(
+        self, client, mocked_admin_not_given_permissions
+    ):
+        response = client.get(
+            "/shared/admin",
+            headers={
+                "X-Endpoint-API-UserInfo": TestAdmin._get_dummy_user_info(
+                    internal_metadata={"isServiceAccount": True}
+                )
+            },
+        )
+        assert response.status_code == 200
