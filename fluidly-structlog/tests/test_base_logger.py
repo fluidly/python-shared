9acc52bd553bbc6cce7876e34b629be4d2ee8bfe
Rob <robert@fluidly.com>
2019-11-05 14:35:58 +0000

    format

--- fluidly-structlog/tests/test_base_logger.py
+++ fluidly-structlog/tests/test_base_logger.py
@@ -4,4 +4,4 @@ from fluidly.structlog.base_logger import get_logger
 def test_get_logger():
     logger = get_logger()
     assert logger is not None
-    logger.info('test logger')
+    logger.info("test logger")
