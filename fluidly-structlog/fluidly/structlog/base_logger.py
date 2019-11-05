8bc06481ea5649f93fcb6d0472da6ce0480c2cdb
Robert Collins <robert.collins@fluidly.com>
2019-11-05 16:28:28 +0000

    Adding severity to base logger (#22)
    * Adding severity to base logger
    
    * format

--- fluidly-structlog/fluidly/structlog/base_logger.py
+++ fluidly-structlog/fluidly/structlog/base_logger.py
@@ -1,9 +1,23 @@
 import structlog
 from structlog.threadlocal import wrap_dict
 
+
+def add_log_level_as_severity(logger, method_name, event_dict):
+    """
+    Add the log level to the event dict.
+    """
+    if method_name == "warn":
+        # The stdlib has an alias
+        method_name = "warning"
+
+    event_dict["severity"] = method_name
+    return event_dict
+
+
 structlog.configure(
     processors=[
         structlog.processors.format_exc_info,
+        add_log_level_as_severity,
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.JSONRenderer(),
     ],
