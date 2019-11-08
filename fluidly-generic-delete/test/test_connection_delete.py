9f66035440fb5aaac8685cbfdaa08d39481790af
TomFluidly <54806200+TomFluidly@users.noreply.github.com>
2019-11-08 13:26:15 +0000

    Delete excluded tables (#25)
    * Add generic delete function to python shared
    
    * Add sql alchemy dependency
    
    * Add ignored tables
    
    * Apply formatting

--- fluidly-generic-delete/test/test_connection_delete.py
+++ fluidly-generic-delete/test/test_connection_delete.py
@@ -71,3 +71,28 @@ def test_delete_by_connection_id_when_table_invalid(mock_session):
 
     assert not mock_session.execute.called
     assert mock_session.commit.called
+
+
+def test_delete_ignores_ignored_tables(mock_session):
+    base_mock = Mock()
+    metadata_mock = Mock()
+    tables_mock = Mock()
+
+    table_mock = Mock()
+    columns = Mock()
+
+    table_mock.c = columns
+    table_mock.name = "ignore"
+    columns.get.side_effect = lambda c: None
+
+    base_mock.metadata = metadata_mock
+    metadata_mock.tables = tables_mock
+    tables_mock.values.return_value = [table_mock]
+
+    consumer = DeleteConnectionConsumer(base_mock, ["ignore"])
+    delete_message = message_from_dict({"connection_id": "test:123"})
+
+    consumer.delete_by_connection_id(mock_session, delete_message, False)
+
+    assert not mock_session.execute.called
+    assert mock_session.commit.called
