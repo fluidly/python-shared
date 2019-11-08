de62948fed445a3447f00b738b09c7fb2bda4055
TomFluidly <54806200+TomFluidly@users.noreply.github.com>
2019-11-08 13:26:15 +0000

    Delete excluded tables (#25)
    * Add generic delete function to python shared
    
    * Add sql alchemy dependency
    
    * Add ignored tables
    
    * Apply formatting

--- fluidly-generic-delete/fluidly/generic_delete/connection_delete.py
+++ fluidly-generic-delete/fluidly/generic_delete/connection_delete.py
@@ -3,14 +3,21 @@ from sqlalchemy import delete
 
 
 class DeleteConnectionConsumer:
-    def __init__(self, base):
+    def __init__(self, base, ignored_tables=None):
         self.base = base
+        if ignored_tables is None:
+            self.ignored_tables = []
+        else:
+            self.ignored_tables = ignored_tables
 
     @pubsub_log_entrypoint_class
     def delete_by_connection_id(self, session, message, refresh_generated=False):
         connection_id = message.connection_id
         for table in self.base.metadata.tables.values():
-            if table.c.get("connection_id") is not None:
+            if (
+                table.name not in self.ignored_tables
+                and table.c.get("connection_id") is not None
+            ):
                 session.execute(
                     delete(table).where(table.c.connection_id == connection_id)
                 )
