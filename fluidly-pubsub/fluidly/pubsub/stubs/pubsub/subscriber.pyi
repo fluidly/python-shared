9178c1b9d5145fb7903084474ec5151610e52c2f
Rob <robert@fluidly.com>
2020-03-19 17:06:57 +0000

    Fixing stubs

---
+++ fluidly-pubsub/fluidly/pubsub/stubs/pubsub/subscriber.pyi
@@ -0,0 +1,9 @@
+from typing import Any
+
+from fluidly.pubsub.base_subscriber import (
+    setup_base_subscriber as setup_base_subscriber,
+)
+
+subscriber: Any
+
+def setup_subscriptions(subscriptions: Any) -> None: ...
