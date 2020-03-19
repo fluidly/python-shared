58ade01d1c513a5cafb517d77627b9270fa7d0d0
Robert Collins <robert.collins@fluidly.com>
2020-03-19 13:57:01 +0000

    Adding typing (#36)
    * Adding typing
    
    * adding typing
    
    Co-authored-by: Rob <robert@fluidly.com>

---
+++ stubs/setup.pyi
@@ -0,0 +1,16 @@
+from typing import Any
+
+NAME: str
+DESCRIPTION: str
+URL: str
+EMAIL: str
+AUTHOR: str
+REQUIRES_PYTHON: str
+VERSION: str
+REQUIRED: Any
+EXTRAS: Any
+package_root: Any
+readme_filename: Any
+readme: Any
+readme = DESCRIPTION
+packages: Any
