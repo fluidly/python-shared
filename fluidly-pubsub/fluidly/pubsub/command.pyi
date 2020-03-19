87b10741ed06041449044bcef44307a0f83ab777
Rob <robert@fluidly.com>
2020-03-19 17:43:20 +0000

    moving stubs to same level as other code

--- fluidly-pubsub/fluidly/pubsub/command.pyi
+++ fluidly-pubsub/fluidly/pubsub/command.pyi
@@ -1,8 +1,5 @@
+from fluidly.pubsub.base_subscriber import setup_base_subscriber as setup_base_subscriber
 from typing import Any, Callable, List, Tuple
-
-from fluidly.pubsub.base_subscriber import (
-    setup_base_subscriber as setup_base_subscriber,
-)
 
 Subscriptions = List[Tuple[str, Callable[[str], Any]]]
 subscriber: Any
