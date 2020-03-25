fbbcb72e4013c9b3208c3a57b361272c2d7df6ee
Rob <robert@fluidly.com>
2020-03-25 16:56:50 +0000

    Add mypy to ci

--- fluidly-sqlalchemy/fluidly/sqlalchemy/upsert.py
+++ fluidly-sqlalchemy/fluidly/sqlalchemy/upsert.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 from sqlalchemy.sql import Insert
 
 
-def get_on_conflict_stmt(stmt: Insert, index, args, where) -> Insert:
+def get_on_conflict_stmt(stmt: Insert, index: Any, args: Any, where: Any) -> Insert:
     return stmt.on_conflict_do_update(
         index_elements=index,
         set_={attr: getattr(stmt.excluded, attr) for attr in args},
@@ -9,7 +11,7 @@ def get_on_conflict_stmt(stmt: Insert, index, args, where) -> Insert:
     )
 
 
-def update_required(normalised_table, stmt, refresh_data):
+def update_required(normalised_table: Any, stmt: Any, refresh_data: bool) -> Any:
     return (
         stmt.excluded.updated_at > normalised_table.c.updated_at
         if not refresh_data
