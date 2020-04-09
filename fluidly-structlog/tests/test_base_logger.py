ca0d4e7e8f3a8c82a3f941d6ef1c9842a4e4e2e7
Tom Phillips <tom.phillips@fluidly.com>
2020-04-09 15:54:31 +0100

    Add filter_by_level

--- fluidly-structlog/tests/test_base_logger.py
+++ fluidly-structlog/tests/test_base_logger.py
@@ -1,7 +1,28 @@
-from fluidly.structlog.base_logger import get_logger
+import pytest
+import structlog
+
+from fluidly.structlog.base_logger import get_logger, filter_by_level
 
 
 def test_get_logger():
     logger = get_logger()
     assert logger is not None
     logger.info("test logger")
+
+
+def test_filter_by_level():
+    logger = object()  # doesn't matter
+    message = {"key": "value"}
+
+    with pytest.raises(structlog.DropEvent):
+        filter_by_level(logger, "debug", message)
+        filter_by_level(logger, "info", message)
+
+    assert filter_by_level(logger, "warn", message)
+    assert filter_by_level(logger, "warning", message)
+    assert filter_by_level(logger, "error", message)
+    assert filter_by_level(logger, "critical", message)
+
+
+def test_filter_by_level_with_unknown_method():
+    assert filter_by_level(object(), "foo", {"key": "value"})  # not dropped
