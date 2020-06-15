504f1645c36f207dc8e71bb057f93e210dece817
Marcus Mo <marcus.mo@fluidly.com>
2020-06-15 12:27:30 +0100

    Add validate_content_type function (#48)

---
+++ fluidly-flask/tests/test_validate_content_type.py
@@ -0,0 +1,73 @@
+from unittest import mock
+
+import pytest
+from flask import Response, request
+
+import fluidly.flask.validate_content_type as module
+from fluidly.flask.validate_content_type import validate_content_type
+
+
+@pytest.fixture()
+def logger_mock(monkeypatch):
+    mock_logger = mock.MagicMock()
+    monkeypatch.setattr(module, "get_logger", lambda: mock_logger)
+    yield mock_logger
+
+
+@pytest.fixture()
+def request_mock(monkeypatch):
+    mock_request = mock.create_autospec(request)
+    mock_request.view_args = {}
+    mock_request.full_path = "/foo"
+    monkeypatch.setattr(module, "request", mock_request)
+    yield mock_request
+
+
+def test_content_type_valid(logger_mock, request_mock):
+    request_mock.headers = {"Accept": "text/plain"}
+
+    response = Response("foobar", content_type="text/plain")
+
+    validated_response = validate_content_type(response)
+
+    assert validated_response == response
+    assert not logger_mock.warning.called
+
+
+def test_content_type_invalid(logger_mock, request_mock):
+    request_mock.headers = {"Accept": "text/plain"}
+
+    response = Response("{}", content_type="application/json")
+
+    validated_response = validate_content_type(response)
+
+    assert validated_response == response
+    assert logger_mock.warning.called
+    logger_mock.warning.assert_called_with(
+        "Incompatible request Accept header and response content-type",
+        args={},
+        headers=request_mock.headers,
+        url="/foo",
+    )
+
+
+def test_no_accept_header(logger_mock, request_mock):
+    request_mock.headers = {}
+
+    response = Response("{}", content_type="application/json")
+
+    validated_response = validate_content_type(response)
+
+    assert validated_response == response
+    assert not logger_mock.warning.called
+
+
+def test_wildcard_accept_header(logger_mock, request_mock):
+    request_mock.headers = {"Accept": "*/*"}
+
+    response = Response("{}", content_type="application/json")
+
+    validated_response = validate_content_type(response)
+
+    assert validated_response == response
+    assert not logger_mock.warning.called
