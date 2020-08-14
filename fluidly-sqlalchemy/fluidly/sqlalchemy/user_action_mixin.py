4db0e3d320e77eef5c33bb6356001c1ac5724069
Jacek Wojna <gastlich@gmail.com>
2020-08-14 15:26:28 +0100

    Add tests

--- fluidly-sqlalchemy/fluidly/sqlalchemy/user_action_mixin.py
+++ fluidly-sqlalchemy/fluidly/sqlalchemy/user_action_mixin.py
@@ -1,9 +1,24 @@
 import sqlalchemy as db
+from sqlalchemy import func
 
 
 class UserActionMixin(object):
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
-    created_by = db.Column(db.Integer)
-    updated_by = db.Column(db.Integer)
+    # Creation
+    created_at = db.Column(
+        db.DateTime, default=func.now(), server_default=func.now(), nullable=False
+    )
+    created_by = db.Column(db.String)
+
+    # Update
+    updated_at = db.Column(
+        db.DateTime,
+        default=func.now(),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+    updated_by = db.Column(db.String)
+
+    # Deletion
     deleted_at = db.Column(db.DateTime)
+    deleted_by = db.Column(db.String)
