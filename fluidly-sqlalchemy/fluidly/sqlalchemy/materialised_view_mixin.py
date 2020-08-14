22b61f72c7cec19f9cbcb9103d94b7c2974ac5b8
gastlich <gastlich@gmail.com>
2020-08-14 15:34:35 +0100

    Update fluidly-sqlalchemy/fluidly/sqlalchemy/materialised_view_mixin.py
    Co-authored-by: Marcus Mo <marcus.mo@fluidly.com>

--- fluidly-sqlalchemy/fluidly/sqlalchemy/materialised_view_mixin.py
+++ fluidly-sqlalchemy/fluidly/sqlalchemy/materialised_view_mixin.py
@@ -2,7 +2,7 @@ import sqlalchemy as db
 from sqlalchemy import func
 
 
-class MaterialisedViewMixin(object):
+class MaterialisedViewMixin:
     view_generated_at = db.Column(
         db.DateTime, default=func.now(), server_default=func.now(), onupdate=func.now()
     )
