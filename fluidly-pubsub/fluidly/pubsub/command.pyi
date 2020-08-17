9eb5770ce38620d297c48c960c63a52ed1e31232
Marcus Mo <marcus.mo@fluidly.com>
2020-08-17 09:23:45 +0100

    Fix setup_subscriptions type hints

--- fluidly-pubsub/fluidly/pubsub/command.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Any, Callable, List, Tuple
-
-Subscriptions = List[Tuple[str, Callable[[str], Any]]]
-subscriber: Any
-
-def setup_subscriptions(subscriptions: Subscriptions, **kwargs: Any) -> Any: ...
