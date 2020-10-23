d566d14a9a9f8ef17d86a4bec6bcb3443b7b8785
Rob <robert@fluidly.com>
2020-10-23 15:33:10 +0100

    adding fake publisher

--- fluidly-pubsub/fluidly/pubsub/fake_publisher.py
+++ fluidly-pubsub/fluidly/pubsub/fake_publisher.py
@@ -1,27 +1,31 @@
 import json
 from collections import defaultdict
+from typing import Any, Callable, Dict, List, Tuple
 
+from fluidly.pubsub.message import Message
 from fluidly.pubsub.tests import message_from_dict
 
 
 class MessageFuture:
-    def result(self):
+    def result(self) -> None:
         pass
 
 
 class TopicSpy:
-    def __init__(self):
+    def __init__(self) -> None:
         self.call_count = 0
-        self.call_list = []
+        self.call_list: List[str] = []
 
 
 class FakePublisher:
-    def __init__(self, session, subscriptions=[]):
+    def __init__(
+        self, session: Any, subscriptions: List[Tuple[str, Callable[..., Message]]] = []
+    ) -> None:
         self.session = session
         self.subscriptions = dict(subscriptions)
-        self.topics_called = defaultdict(TopicSpy)
+        self.topics_called: Dict[str, TopicSpy] = defaultdict(TopicSpy)
 
-    def publish(self, topic, data, connection_id=""):
+    def publish(self, topic: str, data: str, connection_id: str = "") -> MessageFuture:
 
         if topic in self.subscriptions:
             self.subscriptions[topic](self.session, message_from_dict(json.loads(data)))
@@ -29,5 +33,5 @@ class FakePublisher:
         self.topics_called[topic].call_list.append(data)
         return MessageFuture()
 
-    def topic_path(self, project, topic):
+    def topic_path(self, project: str, topic: str) -> str:
         return topic
