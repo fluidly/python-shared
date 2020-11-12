349ba7e841ae4edef5a0bf7a5266636e9dfd536d
Marcus Mo <marcus.mo@fluidly.com>
2020-11-12 08:56:24 +0000

    Support requests with no view args (#109)

--- fluidly-flask/tests/test_validate_content_type.py
+++ fluidly-flask/tests/test_validate_content_type.py
@@ -23,6 +23,12 @@ def request_mock(monkeypatch):
     yield mock_request
 
 
+@pytest.fixture()
+def request_mock_with_no_view_args(request_mock):
+    request_mock.view_args = None
+    yield request_mock
+
+
 def test_content_type_valid(logger_mock, request_mock):
     request_mock.headers = {"Accept": "text/plain"}
 
@@ -109,3 +115,15 @@ def test_subset_content_type(logger_mock, request_mock):
 
     validate_content_type(response)
     assert not logger_mock.warning.called
+
+
+def test_invalid_request_with_no_view_args(logger_mock, request_mock_with_no_view_args):
+    request_mock_with_no_view_args.headers = {"Accept": "text/plain"}
+
+    response = Response("{}", content_type="application/json")
+
+    validate_content_type(response)
+    assert logger_mock.warning.called
+
+    args, kwargs = logger_mock.warning.call_args
+    assert kwargs["args"] is None
