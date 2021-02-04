dafdfe0ef8c87376134804345d1f0c8af0987a04
Marcus Mo <marcus.mo@fluidly.com>
2021-02-04 11:56:58 +0000

    configure nox (#125)
    * Ignore nox venvs
    
    * Add noxfile
    
    * Remove postgres from CI
    
    * Update circleci to run nox
    
    * Remove dockerfile and pipfile
    
    * Change working directory
    
    * Rename
    
    * Format
    
    * Fix session name
    
    * Rename
    
    * Add typing
    
    * Format
    
    * Add more typing
    
    * Fix command
    
    * Configure pre-commit to use nox
    
    * Dynamically find fluidly packages
    
    * Add pyproject.toml files
    
    * Update readme with more information
    
    * MASSIVE HACK: do not install local dependencies in tests
    
    * Use a better name for the environment variable

--- fluidly-flask/setup.py
+++ fluidly-flask/setup.py
@@ -5,7 +5,6 @@ import os
 
 from setuptools import find_packages, setup
 
-# Package meta-data.
 NAME = "fluidly-flask"
 DESCRIPTION = "Flask helpers."
 URL = "https://github.com/fluidly/python-shared"
@@ -14,22 +13,25 @@ AUTHOR = "Fluidly"
 REQUIRES_PYTHON = ">=3.6.0"
 VERSION = "0.1.0"
 
-# What packages are required for this module to be executed?
+
+def local_dependencies(*packages):
+    if os.environ.get("INSTALL_EDITABLE"):
+        return []
+
+    return [
+        f"{package} @ git+ssh://git@github.com/fluidly/python-shared.git#subdirectory={package}"
+        for package in packages
+    ]
+
+
 REQUIRED = [
     "flask",
     "marshmallow",
     "uwsgi",
-    "fluidly-structlog @ git+ssh://git@github.com/fluidly/python-shared.git#subdirectory=fluidly-structlog",
-    "fluidly-auth @ git+ssh://git@github.com/fluidly/python-shared.git#subdirectory=fluidly-auth",
-]
+] + local_dependencies("fluidly-structlog", "fluidly-auth")
 
 
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# Setup boilerplate below this line.
+EXTRAS = {}
 
 try:
     package_root = os.path.abspath(os.path.dirname(__file__))
