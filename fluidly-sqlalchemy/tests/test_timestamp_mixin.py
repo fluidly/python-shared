d38abeee7d16be45557a66e8f83a5556c78c7e5b
Jacek Wojna <gastlich@gmail.com>
2021-03-04 12:24:54 +0000

    Allow DO NOTHING in get_on_conflict_stmt

--- fluidly-sqlalchemy/tests/test_timestamp_mixin.py
+++ fluidly-sqlalchemy/tests/test_timestamp_mixin.py
@@ -16,12 +16,6 @@ class SomeModel(TimestampMixin, Base):
     id = db.Column(db.Integer, primary_key=True)
 
 
-class AnotherModel(Base):
-    __tablename__ = "another_model"
-
-    id = db.Column(db.Integer, primary_key=True)
-
-
 def format_statement(s):
     try:
         s = s.compile(
@@ -47,28 +41,4 @@ def test_insert():
     assert (
         format_statement(stmt)
         == "INSERT INTO some_model (last_seen_at, first_seen_at, id) VALUES (now(), now(), 1)"
-    )
-
-
-def test_upsert():
-    table = SomeModel.__table__
-    stmt = insert(table).values(id=1)
-    stmt = get_on_conflict_stmt(
-        stmt, ["id"], ["id"], where=stmt.excluded.id == table.c.id
-    )
-    assert (
-        format_statement(stmt)
-        == "INSERT INTO some_model (last_seen_at, first_seen_at, id) VALUES (now(), now(), 1) ON CONFLICT (id) DO UPDATE SET last_seen_at = %(param_1)s, id = excluded.id WHERE excluded.id = some_model.id"
-    )
-
-
-def test_upsert_without_timestamp_mixin():
-    table = AnotherModel.__table__
-    stmt = insert(table).values(id=1)
-    stmt = get_on_conflict_stmt(
-        stmt, ["id"], ["id"], where=stmt.excluded.id == table.c.id
-    )
-    assert (
-        format_statement(stmt)
-        == "INSERT INTO another_model (id) VALUES (1) ON CONFLICT (id) DO UPDATE SET id = excluded.id WHERE excluded.id = another_model.id"
     )
--- fluidly-sqlalchemy/tests/test_upsert.py
+++ fluidly-sqlalchemy/tests/test_upsert.py
@@ -1,40 +1,104 @@
 from unittest import mock
+from fluidly.sqlalchemy.timestamp_mixin import TimestampMixin
 
-from fluidly.sqlalchemy.upsert import get_on_conflict_stmt, update_required
+from fluidly.sqlalchemy.upsert import (
+    ConflictAction,
+    get_on_conflict_stmt,
+    update_required,
+)
+import sqlalchemy as db
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.declarative import declarative_base
 
-
-def test_get_on_conflict_stmt():
-    stmt = mock.MagicMock()
-    index = mock.MagicMock()
-    where = mock.MagicMock()
-
-    stmt.excluded.argument = "Test"
-    stmt.excluded.another_argument = "Test"
-
-    get_on_conflict_stmt(stmt, index, ["argument", "another_argument"], where)
-
-    args_list = stmt.on_conflict_do_update.call_args_list
-
-    assert stmt.on_conflict_do_update.called
-    assert args_list[0][1]["index_elements"] == index
-    assert args_list[0][1]["set_"]["argument"] == "Test"
-    assert args_list[0][1]["where"] == where
+Base = declarative_base()
 
 
-def test_get_on_conflict_stmt_do_nothing():
-    stmt = mock.MagicMock()
-    index = mock.MagicMock()
-    where = mock.MagicMock()
+class SomeModel(Base):
+    __tablename__ = "some_model"
 
-    get_on_conflict_stmt(stmt, index, [], where)
+    id = db.Column(db.Integer, primary_key=True)
 
-    assert not stmt.on_conflict_do_update.called
 
-    assert stmt.on_conflict_do_nothing.called
+class TimestampModel(TimestampMixin, Base):
+    __tablename__ = "timestamp_model"
 
-    args_list = stmt.on_conflict_do_nothing.call_args_list
+    id = db.Column(db.Integer, primary_key=True)
 
-    assert args_list[0][1]["index_elements"] == index
+
+def format_statement(s):
+    try:
+        s = s.compile(
+            dialect=postgresql.dialect(), compile_kwargs={"literal_binds": True}
+        )
+    except AttributeError:
+        s = str(s).replace("\n", "")
+
+    return str(s)
+
+
+class TestGetOnConflictStmt:
+    def test_upsert(self):
+        some_model_table = SomeModel.__table__
+
+        stmt = insert(some_model_table).values(id=1)
+        stmt = get_on_conflict_stmt(
+            stmt, ["id"], ["id"], where=stmt.excluded.id == some_model_table.c.id
+        )
+        assert (
+            format_statement(stmt)
+            == "INSERT INTO some_model (id) VALUES (1) ON CONFLICT (id) DO UPDATE SET id = excluded.id WHERE excluded.id = some_model.id"
+        )
+
+    def test_upsert_with_timestamp_mixin(self):
+        timestamp_model_table = TimestampModel.__table__
+
+        stmt = insert(timestamp_model_table).values(id=1)
+        stmt = get_on_conflict_stmt(
+            stmt, ["id"], ["id"], where=stmt.excluded.id == timestamp_model_table.c.id
+        )
+        assert (
+            format_statement(stmt)
+            == "INSERT INTO timestamp_model (last_seen_at, first_seen_at, id) VALUES (now(), now(), 1) ON CONFLICT (id) DO UPDATE SET last_seen_at = %(param_1)s, id = excluded.id WHERE excluded.id = timestamp_model.id"
+        )
+
+    def test_does_nothing_if_no_arguments(self):
+        some_model_table = SomeModel.__table__
+
+        stmt = insert(some_model_table).values(id=1)
+        stmt = get_on_conflict_stmt(
+            stmt, ["id"], [], where=stmt.excluded.id == some_model_table.c.id
+        )
+        assert (
+            format_statement(stmt)
+            == "INSERT INTO some_model (id) VALUES (1) ON CONFLICT (id) DO NOTHING"
+        )
+
+    def test_does_nothing_if_do_nothing_action_was_passed(self):
+        some_model_table = SomeModel.__table__
+
+        stmt = insert(some_model_table).values(id=1)
+        stmt = get_on_conflict_stmt(
+            stmt,
+            ["id"],
+            ["id"],
+            where=stmt.excluded.id == some_model_table.c.id,
+            action=ConflictAction.DO_NOTHING,
+        )
+        assert (
+            format_statement(stmt)
+            == "INSERT INTO some_model (id) VALUES (1) ON CONFLICT (id) DO NOTHING"
+        )
+
+    def test_not_passing_where_clause(self):
+        some_model_table = SomeModel.__table__
+
+        stmt = insert(some_model_table).values(id=1)
+        stmt = get_on_conflict_stmt(stmt, ["id"], ["id"])
+        assert (
+            format_statement(stmt)
+            == "INSERT INTO some_model (id) VALUES (1) ON CONFLICT (id) DO UPDATE SET id = excluded.id"
+        )
 
 
 def test_update_required_refresh_data_true_return_false():
