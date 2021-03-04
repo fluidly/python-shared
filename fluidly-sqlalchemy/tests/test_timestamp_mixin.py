31414223ace0da6bdcf648584842dd6ab4da48cb
gastlich <gastlich@gmail.com>
2021-03-04 15:31:37 +0100

    Allow do nothing (#128)
    * Allow DO NOTHING in get_on_conflict_stmt
    
    * Allow passing ConflictAction to upsert_entity

--- fluidly-sqlalchemy/tests/test_timestamp_mixin.py
+++ fluidly-sqlalchemy/tests/test_timestamp_mixin.py
@@ -5,19 +5,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from fluidly.sqlalchemy.db import db_session
 from fluidly.sqlalchemy.timestamp_mixin import TimestampMixin
-from fluidly.sqlalchemy.upsert import get_on_conflict_stmt
 
 Base = declarative_base()
 
 
 class SomeModel(TimestampMixin, Base):
     __tablename__ = "some_model"
-
-    id = db.Column(db.Integer, primary_key=True)
-
-
-class AnotherModel(Base):
-    __tablename__ = "another_model"
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -47,28 +40,4 @@ def test_insert():
     assert (
         format_statement(stmt)
         == "INSERT INTO some_model (last_seen_at, first_seen_at, id) VALUES (now(), now(), 1)"
-    )
-
-
-def test_upsert():
-    table = SomeModel.__table__
-    stmt = insert(table).values(id=1)
-    stmt = get_on_conflict_stmt(
-        stmt, ["id"], ["id"], where=stmt.excluded.id == table.c.id
-    )
-    assert (
-        format_statement(stmt)
-        == "INSERT INTO some_model (last_seen_at, first_seen_at, id) VALUES (now(), now(), 1) ON CONFLICT (id) DO UPDATE SET last_seen_at = %(param_1)s, id = excluded.id WHERE excluded.id = some_model.id"
-    )
-
-
-def test_upsert_without_timestamp_mixin():
-    table = AnotherModel.__table__
-    stmt = insert(table).values(id=1)
-    stmt = get_on_conflict_stmt(
-        stmt, ["id"], ["id"], where=stmt.excluded.id == table.c.id
-    )
-    assert (
-        format_statement(stmt)
-        == "INSERT INTO another_model (id) VALUES (1) ON CONFLICT (id) DO UPDATE SET id = excluded.id WHERE excluded.id = another_model.id"
     )
