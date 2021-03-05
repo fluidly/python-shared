f52430c0f7b0dd5f436cad2d5aba0aa9635c8e22
Adam <adam.gibson@fluidly.com>
2021-03-05 16:40:19 +0000

    add email to jwt request headers (#130)

--- fluidly-auth/fluidly/auth/tests.py
+++ fluidly-auth/fluidly/auth/tests.py
@@ -2,27 +2,28 @@ import json
 from base64 import b64encode
 
 
-def request_headers(if_modified_since=None, service_account=False):
+def request_headers(if_modified_since=None, service_account=False, email=None):
     # Claims attribute is in string format in the header provided by Cloud Endpoints
-    claims = json.dumps(
-        {
-            "https://api.fluidly.com/internal_metadata": {
-                "isServiceAccount": service_account
-            },
-            "https://api.fluidly.com/user_metadata": {},
-            "https://api.fluidly.com/app_metadata": {"userId": "1"},
-            "iss": "https://fluidly.eu.auth0.com/",
-            "sub": "auth0|5cb644d401c03f119f1ed772",
-            "aud": ["https://api.fluidly.com", "https://fluidly.eu.auth0.com/userinfo"],
-            "iat": 1555519624,
-            "exp": 1555526824,
-            "azp": "5HIxj76CRK3QCxUa2KlUwJw7vBCly9e5",
-            "scope": "openid email",
-        }
-    )
+    claims = {
+        "https://api.fluidly.com/internal_metadata": {
+            "isServiceAccount": service_account
+        },
+        "https://api.fluidly.com/user_metadata": {},
+        "https://api.fluidly.com/app_metadata": {"userId": "1"},
+        "iss": "https://fluidly.eu.auth0.com/",
+        "sub": "auth0|5cb644d401c03f119f1ed772",
+        "aud": ["https://api.fluidly.com", "https://fluidly.eu.auth0.com/userinfo"],
+        "iat": 1555519624,
+        "exp": 1555526824,
+        "azp": "5HIxj76CRK3QCxUa2KlUwJw7vBCly9e5",
+        "scope": "openid email",
+    }
+    if email:
+        claims["email"] = email
+
     endpoint_header = json.dumps(
         {
-            "claims": claims,
+            "claims": json.dumps(claims),
             "issuer": "https://fluidly.eu.auth0.com/",
             "id": "auth0|5cb644d401c03f119f1ed772",
         }
