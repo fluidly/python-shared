b5b48fa07a9490533b754309ce71956032d53565
Marcus Mo <marcus.mo@fluidly.com>
2021-03-18 16:50:10 +0000

    Add types to Pub/Sub module (#132)

--- fluidly-pubsub/fluidly/pubsub/subscriber.py
+++ fluidly-pubsub/fluidly/pubsub/subscriber.py
@@ -1,9 +1,9 @@
 from google.cloud import pubsub_v1
 
-from fluidly.pubsub.base_subscriber import setup_base_subscriber
+from fluidly.pubsub.base_subscriber import Subscriptions, setup_base_subscriber
 
 subscriber = pubsub_v1.SubscriberClient()
 
 
-def setup_subscriptions(subscriptions):
+def setup_subscriptions(subscriptions: Subscriptions) -> None:
     setup_base_subscriber(subscriber, subscriptions)
--- fluidly-pubsub/fluidly/pubsub/subscriber.pyi
+++
@@ -1,5 +0,0 @@
-from typing import Any
-
-subscriber: Any
-
-def setup_subscriptions(subscriptions: Any) -> None: ...
