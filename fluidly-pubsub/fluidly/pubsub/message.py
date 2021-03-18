3f0581f4cfc28e8a2025959e0abc9c4e241df17a
Marcus Mo <marcus.mo@fluidly.com>
2021-03-18 16:03:11 +0000

    Add types to Pub/Sub module

--- fluidly-pubsub/fluidly/pubsub/message.py
+++ fluidly-pubsub/fluidly/pubsub/message.py
@@ -1,16 +1,19 @@
 import json
+from typing import Any
+
+from google.cloud.pubsub_v1.types import PubsubMessage
 
 from fluidly.pubsub.exceptions import DropMessageException
 
 
 class Message:
-    def __init__(self, message):
+    def __init__(self, message: PubsubMessage) -> None:
         self.message = message
         self.attributes = dict(message.attributes) if message.attributes else {}
         self.data = json.loads(message.data)
         self.publish_time = message.publish_time
 
-    def __getattr__(self, attr):
+    def __getattr__(self, attr: str) -> Any:
         try:
             return self.data[attr]
         except KeyError as e:
