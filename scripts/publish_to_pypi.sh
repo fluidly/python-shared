68b343a9bcbeffe42f1c2a1a591fb643631ba3b6
Marcus Mo <marcus.mo@fluidly.com>
2021-08-27 15:53:00 +0100

    Publish all packages (#146)

--- scripts/publish_to_pypi.sh
+++ scripts/publish_to_pypi.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cd fluidly-structlog
-pipenv run python setup.py sdist bdist_wheel
-pipenv run twine upload --skip-existing dist/*
+DIRS=$(find . -type d -name "fluidly-*")
+
+for DIR in $DIRS
+do
+    cd $DIR
+    pipenv run python setup.py sdist bdist_wheel
+    pipenv run twine upload --skip-existing dist/*
+    cd -
+done
