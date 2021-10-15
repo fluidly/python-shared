be342ab6319d42c30799dc7b9033ef34b2c85146
Marcus Mo <marcus.mo@fluidly.com>
2021-10-15 14:29:34 +0100

    Return Pub/Sub subscription futures

--- fluidly-pubsub/fluidly/pubsub/command.py
+++ fluidly-pubsub/fluidly/pubsub/command.py
@@ -1,14 +1,17 @@
-from typing import Any, Callable, List, Tuple
+from typing import Any
 
 from google.cloud import pubsub_v1
 
-from fluidly.pubsub.base_subscriber import setup_base_subscriber
-from fluidly.pubsub.message import Message
-
-Subscriptions = List[Tuple[str, Callable[[Message], Any]]]
+from fluidly.pubsub.base_subscriber import (
+    SubscriptionFutures,
+    Subscriptions,
+    setup_base_subscriber,
+)
 
 subscriber = pubsub_v1.SubscriberClient()
 
 
-def setup_subscriptions(subscriptions: Subscriptions, **kwargs: Any) -> None:
-    setup_base_subscriber(subscriber, subscriptions, **kwargs)
+def setup_subscriptions(
+    subscriptions: Subscriptions, **kwargs: Any
+) -> SubscriptionFutures:
+    return setup_base_subscriber(subscriber, subscriptions, **kwargs)
