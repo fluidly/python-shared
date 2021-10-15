244e0cf5046b00dedd2f57d6fe3cd27f2d1db05e
Marcus Mo <marcus.mo@fluidly.com>
2021-10-15 14:29:15 +0100

    Bump version: 0.1.2 → 0.1.3

--- fluidly-pubsub/setup.py
+++ fluidly-pubsub/setup.py
@@ -11,7 +11,7 @@ URL = "https://github.com/fluidly/python-shared"
 EMAIL = "tech@fluidly.com"
 AUTHOR = "Fluidly"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 REQUIRED = ["google-cloud-pubsub"]
 
