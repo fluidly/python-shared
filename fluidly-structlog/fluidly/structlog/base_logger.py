3f11e74b7e8d7f210839accfd70ee1ddf2d5951d
Thomas Ormezzano <thormezz@gmail.com>
2021-11-01 11:06:30 +0000

    move configuration to get_logger

--- fluidly-structlog/fluidly/structlog/base_logger.py
+++ fluidly-structlog/fluidly/structlog/base_logger.py
@@ -53,31 +53,28 @@ def add_service_context(logger: Any, method_name: str, event_dict: Dict) -> Dict
     return event_dict
 
 
-structlog.configure(
-    processors=[
-        filter_by_level,
-        structlog.processors.format_exc_info,
-        add_log_level_as_severity,
-        add_service_context,
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.dev.ConsoleRenderer()
-        if "PRETTY_LOGS" in os.environ
-        else structlog.processors.JSONRenderer(),
-    ],
-    context_class=wrap_dict(dict),
-)
-
-get_logger = structlog.get_logger
-
-
 def unhandled_exception_hook(exception_type, exception, traceback):
+    """Hook to make sure the log processors are applied to unhandled exceptions as well"""
     logger = get_logger()
     logger.exception(
         "Unhandled Exception", exc_info=(exception_type, exception, traceback)
     )
 
 
-def setup_logging():
-    """Add a sys.excepthook so that the log processors are applied and there is nice
-    error log in gcp"""
-    sys.excepthook = unhandled_exception_hook
+def get_logger():
+    if not structlog.is_configured():
+        structlog.configure(
+            processors=[
+                filter_by_level,
+                structlog.processors.format_exc_info,
+                add_log_level_as_severity,
+                add_service_context,
+                structlog.processors.TimeStamper(fmt="iso"),
+                structlog.dev.ConsoleRenderer()
+                if "PRETTY_LOGS" in os.environ
+                else structlog.processors.JSONRenderer(),
+            ],
+            context_class=wrap_dict(dict),
+        )
+        sys.excepthook = unhandled_exception_hook  # Overriding the default excepthook for better logging of unhandled exceptions
+    return structlog.get_logger()
