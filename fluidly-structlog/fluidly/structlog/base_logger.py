dad998d5cebd77238d10c0a1fc9861e3cfb00d26
tzzh <30912311+tzzh@users.noreply.github.com>
2021-11-01 11:19:02 +0000

    add serviceContext.service to logs for nicer error reporting (#153)
    * add serviceContext.service to logs for nicer error reporting
    
    * use APPLICATION_NAME env variable
    
    * fix typo
    
    * move configuration to get_logger

--- fluidly-structlog/fluidly/structlog/base_logger.py
+++ fluidly-structlog/fluidly/structlog/base_logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 from typing import Any, Dict, Optional
 
 import structlog
@@ -22,7 +23,7 @@ def filter_by_level(
         "WARN": logging.WARN,
         "WARNING": logging.WARNING,
         "ERROR": logging.ERROR,
-        "CRITCAL": logging.CRITICAL,
+        "CRITICAL": logging.CRITICAL,
     }  # type: Dict[str, int]
     try:
         if level[method_name.upper()] < level[min_level]:
@@ -44,17 +45,36 @@ def add_log_level_as_severity(logger: Any, method_name: str, event_dict: Dict) -
     return event_dict
 
 
-structlog.configure(
-    processors=[
-        filter_by_level,
-        structlog.processors.format_exc_info,
-        add_log_level_as_severity,
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.dev.ConsoleRenderer()
-        if "PRETTY_LOGS" in os.environ
-        else structlog.processors.JSONRenderer(),
-    ],
-    context_class=wrap_dict(dict),
-)
+def add_service_context(logger: Any, method_name: str, event_dict: Dict) -> Dict:
+    """Add serviceContext.service if the env variable APPLICATION_NAME is set for error reporting in gcp"""
+    service_name = os.getenv("APPLICATION_NAME")
+    if service_name:
+        event_dict["serviceContext"] = {"service": service_name}
+    return event_dict
 
-get_logger = structlog.get_logger
+
+def unhandled_exception_hook(exception_type, exception, traceback):
+    """Hook to make sure the log processors are applied to unhandled exceptions as well"""
+    logger = get_logger()
+    logger.exception(
+        "Unhandled Exception", exc_info=(exception_type, exception, traceback)
+    )
+
+
+def get_logger():
+    if not structlog.is_configured():
+        structlog.configure(
+            processors=[
+                filter_by_level,
+                structlog.processors.format_exc_info,
+                add_log_level_as_severity,
+                add_service_context,
+                structlog.processors.TimeStamper(fmt="iso"),
+                structlog.dev.ConsoleRenderer()
+                if "PRETTY_LOGS" in os.environ
+                else structlog.processors.JSONRenderer(),
+            ],
+            context_class=wrap_dict(dict),
+        )
+        sys.excepthook = unhandled_exception_hook  # Overriding the default excepthook for better logging of unhandled exceptions
+    return structlog.get_logger()
