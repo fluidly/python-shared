219010419d7d5537d53747f1c378c1db43ce2d8d
Thomas Ormezzano <thormezz@gmail.com>
2022-02-16 15:44:03 +0000

    remove pubsub_helper.pyi

--- fluidly-structlog/fluidly/structlog/pubsub_helper.pyi
+++
@@ -1,8 +0,0 @@
-from typing import TypeVar
-
-from fluidly.structlog.base_logger import get_logger as get_logger
-
-T = TypeVar("T")
-
-def pubsub_log_entrypoint(func: T) -> T: ...
-def pubsub_log_entrypoint_class(func: T) -> T: ...
